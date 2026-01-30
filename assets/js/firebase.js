bd3cf5d6ca35c87ab9a84940d1ef77b422a56b39
minforever557-netizen <minforever557@gmail.com>
2026-01-30 11:23:37 +0700

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,17 +1,22 @@
+// assets/js/firebase.js
 import { initializeApp } from
 "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+
 import { getAuth } from
 "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
 import { getFirestore } from
 "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "sale-support-system.firebaseapp.com",
-  projectId: "sale-support-system",
+    apiKey: "AIzaSy....",
+    authDomain: "sale-support-system.firebaseapp.com",
+    projectId: "sale-support-system",
 };
 
+// Init
 const app = initializeApp(firebaseConfig);
 
+// Export to window (สำคัญ)
 window.auth = getAuth(app);
-window.db = getFirestore(app);
+window.db   = getFirestore(app);
