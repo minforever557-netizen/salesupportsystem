927110531d950d3941f7ed96f229a69d3ff6cd55
minforever557-netizen <minforever557@gmail.com>
2026-01-30 11:06:02 +0700

    Create firebase.js

---
+++ assets/js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsUhBsWjZfQuJo5lgprYt3xikQUyf8iiw",
+  authDomain: "sale-support-system.firebaseapp.com",
+  projectId: "sale-support-system",
+  storageBucket: "sale-support-system.firebasestorage.app",
+  messagingSenderId: "640337786680",
+  appId: "1:640337786680:web:fd80699ac291b8d7a7b3cd"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
