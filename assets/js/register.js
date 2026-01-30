a443454de6292e6f0504a762155ed9c66e05debb
minforever557-netizen <minforever557@gmail.com>
2026-01-30 11:46:07 +0700

    Update register.js

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,28 +1,38 @@
-import { auth, db } from "./firebase.js";
-import { createUserWithEmailAndPassword }
-from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import {
+    createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
-import { doc, setDoc }
-from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import {
+    doc, setDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 document.getElementById("registerBtn").onclick = async () => {
 
     const email = document.getElementById("email").value;
     const pass  = document.getElementById("password").value;
     const role  = document.getElementById("role").value;
+    const msg   = document.getElementById("msg");
 
     try {
-        const cred = await createUserWithEmailAndPassword(auth, email, pass);
-
-        await setDoc(doc(db, "users", cred.user.uid), {
+        const cred = await createUserWithEmailAndPassword(
+            window.auth,
             email,
-            role,
-            createdAt: new Date()
-        });
+            pass
+        );
 
-        location.href = "index.html";
+        await setDoc(
+            doc(window.db, "users", cred.user.uid),
+            {
+                email,
+                role,
+                createdAt: new Date()
+            }
+        );
+
+        msg.innerText = "✅ Register สำเร็จ";
+        setTimeout(() => location.href = "index.html", 1000);
 
     } catch (err) {
-        document.getElementById("msg").innerText = err.message;
+        msg.innerText = err.message;
     }
 };
