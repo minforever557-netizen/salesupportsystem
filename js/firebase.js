d962168a0f50098ffb2f109a4ac3cbf340d77756
minforever557-netizen <minforever557@gmail.com>
2026-01-31 12:15:37 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,20 +1,22 @@
-<script type="module">
+// ==================================================
+// FIREBASE INIT (ใช้ร่วมกันทั้งเว็บ)
+// ==================================================
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-
+// ================= FIREBASE CONFIG =================
 const firebaseConfig = {
-apiKey: "AIzaSyCsUhBsWjZfQuJo5lgprYt3xikQUyf8iiw",
-authDomain: "sale-support-system.firebaseapp.com",
-projectId: "sale-support-system",
-storageBucket: "sale-support-system.appspot.com",
-messagingSenderId: "640337786680",
-appId: "1:640337786680:web:fd80699ac291b8d7a7b3cd"
+  apiKey: "AIzaSyCsUhBsWjZfQuJo5lgprYt3xikQUyf8iiw",
+  authDomain: "sale-support-system.firebaseapp.com",
+  projectId: "sale-support-system",
+  storageBucket: "sale-support-system.appspot.com",
+  messagingSenderId: "640337786680",
+  appId: "1:640337786680:web:fd80699ac291b8d7a7b3cd"
 };
 
-
+// ================= INIT =================
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-</script>
